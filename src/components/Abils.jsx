bfa66b50026f42d03fcf8491167782e61a1e18a2
Ryan Buchmeier <rwbleo@gmail.com>
2017-12-13 16:21:33 -0700

    Added point buy feature

--- src/components/Abils.jsx
+++ src/components/Abils.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import point_buy from '../resources/point_buy.js';
 
 class Abils extends React.Component {
     getTotal(key) {
@@ -6,6 +7,15 @@ class Abils extends React.Component {
         total += parseInt(this.props.abils[key].bonus);
         total += parseInt(this.props.abils[key].racial ? 2 : 0);
         return total;
+    }
+    getPointsUsed(abils) {
+        let points = 0;
+        Object.keys(abils).map((key, index) => {
+            console.log(point_buy);
+            console.log(abils[key].score);
+            points += point_buy[abils[key].score];
+        });
+        return points;
     }
     render() {
         var abilities = this.props.abils;
@@ -20,9 +30,9 @@ class Abils extends React.Component {
                                 <div key={index}>
                                     <label>
                                         {abilities[key].name}
-                                        <input type="number" name={key} value={abilities[key].score} onChange={(e) => _this.props.handleAbilChange(e.target)} />
+                                        <input type="number" min={1} max={20} name={key} value={abilities[key].score} onChange={(e) => _this.props.handleAbilChange(e.target)} />
                                         <input type="checkbox" name={key} checked={abilities[key].racial} onChange={(e) => _this.props.handleRacialChange(e.target)} />
-                                        <input type="number" name={key} value={abilities[key].bonus} onChange={(e) => _this.props.handleBonusChange(e.target)} />
+                                        <input type="number" min={-10} max={10} name={key} value={abilities[key].bonus} onChange={(e) => _this.props.handleBonusChange(e.target)} />
                                         <span className="total">{_this.getTotal(key)}</span>
                                     </label>
                                     <br />
@@ -31,6 +41,7 @@ class Abils extends React.Component {
                         })
                     }
                 </form>
+                <p className='point_buy'>You have used {_this.getPointsUsed(abilities)}/80.</p>
             </div>
         )
     }
